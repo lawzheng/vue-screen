6afc2a4c702ec5b41b7c02d51539fac4149a1ff0
lawzheng <602285510@qq.com>
2020-12-27 21:05:40 +0800

    add flybox components

--- src/App.vue
+++ src/App.vue
@@ -1,52 +1,14 @@
 <template>
-  <div>
-    <loading width="100" height="100">
-      <div>数据大屏加载中...</div>
-    </loading>
+  <div id="app">
+    <router-view></router-view>
   </div>
-<!--  <div id="nav">-->
-<!--    <router-link to="/">Home</router-link> |-->
-<!--    <router-link to="/about">About</router-link> |-->
-<!--    <router-link to="/test">test</router-link>-->
-<!--    <test-component/>-->
-<!--    <svg width="30" height="30">-->
-<!--      <use href="#more"></use>-->
-<!--    </svg>-->
-<!--    <svg width="30" height="30">-->
-<!--      <use href="#play"></use>-->
-<!--    </svg>-->
-<!--    <svg width="30" height="30">-->
-<!--      <use href="#right"></use>-->
-<!--    </svg>-->
-<!--    <Icon prefix="" name="right" :style="{width: '30px', height: '30px', color: 'red'}"></Icon>-->
-<!--    <Icon prefix="" name="box" :style="{width: '30px', height: '30px', color: 'red'}"></Icon>-->
-<!--    <Icon name="qq" :style="{width: '30px', height: '30px', color: 'blue'}"></Icon>-->
-<!--    <test-component-2/>-->
-<!--    <test-component-three/>-->
-<!--    <svg-animation />-->
-<!--  </div>-->
-<!--  <router-view/>-->
 </template>
 
 <style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+html, body, #app {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <container :options="{ width: 3840, height: 2160}">
+      222
+    </container>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
+<style lang="less">
+  #app {
+  }
+</style>
